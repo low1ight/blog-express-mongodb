f40b17675799c83c34ad7b0bc8bb850e7f07c7d2
Maksym <qlowlight@gmail.com>
2025-10-13 02:22:09 +0300

    add isMongoId validator

--- src/modules/users_module/devices/controllers/devices.controller.ts
+++ src/modules/users_module/devices/controllers/devices.controller.ts
@@ -3,6 +3,8 @@ import {jwtRefreshTokenAuthGuard} from "../../auth/guards/jwtRefreshToken.auth.g
 import {getAllUserDevicesHandler} from "./handlers/getAllUserDevicesHandler";
 import {deleteAllOtherUserDevices} from "./handlers/deleteAllOtherUserDevices";
 import {deleteDeviceById} from "./handlers/deleteDeviceById";
+import {validate} from "../../../../common/validator/validationHandler";
+import {idParamValidator} from "../../../../common/validator/mongodb-id-validator";
 
 
 export const deviceRouter = Router();
@@ -10,4 +12,4 @@ export const deviceRouter = Router();
 
 deviceRouter.get('/', jwtRefreshTokenAuthGuard ,getAllUserDevicesHandler)
 deviceRouter.delete('/', jwtRefreshTokenAuthGuard ,deleteAllOtherUserDevices)
-deviceRouter.delete('/:id', jwtRefreshTokenAuthGuard ,deleteDeviceById)
+deviceRouter.delete('/:id', jwtRefreshTokenAuthGuard, validate(idParamValidator) ,deleteDeviceById)
