e34f12506759e1c42c1ec65cd4e5bb88c011e19f
Maksym <qlowlight@gmail.com>
2025-10-18 23:37:35 +0300

    edit post view model, now it returns blogId

--- src/modules/blog_platform/posts/features/toPostViewModel.ts
+++ src/modules/blog_platform/posts/features/toPostViewModel.ts
@@ -1,11 +1,13 @@
 import {PostDocumentModel} from "../models/post-document-model";
+import {PostViewModel} from "../models/post-view-model";
 
-export const toPostViewModel = ({_id,title,shortDescription,content,blogName,createdAt}:PostDocumentModel) => {
+export const toPostViewModel = ({_id,title,shortDescription,content,blogId,blogName,createdAt}:PostDocumentModel):PostViewModel => {
     return {
         id: _id.toString(),
         title,
         shortDescription,
         content,
+        blogId:blogId.toString(),
         blogName,
         createdAt
 
--- src/modules/blog_platform/posts/models/post-view-model.ts
+++ src/modules/blog_platform/posts/models/post-view-model.ts
@@ -3,6 +3,7 @@ export type PostViewModel = {
     title: string;
     shortDescription: string;
     content: string;
+    blogId:string
     blogName:string
     createdAt: string;
 }
