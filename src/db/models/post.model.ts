0968ef53d42493c8c944352c7825cc610b945e3f
Maksym <qlowlight@gmail.com>
2025-10-22 11:53:25 +0300

    add Post model

---
+++ src/db/models/post.model.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import {PostDocumentModel} from "../../modules/blog_platform/posts/models/post-document-model";
+import {ObjectId} from "mongodb";
+
+const POST_COLLECTION_NAME = 'posts';
+
+export const postSchema = new mongoose.Schema<PostDocumentModel>({
+    title: {type: String, required: true},
+    shortDescription: {type: String, required: true},
+    content: {type: String, required: true},
+    blogId: {type: ObjectId, required: true},
+    blogName:{type: String, required: true},
+    createdAt: {type: String, required: true},
+    updatedAt: {type: String, required: true},
+    }, {timestamps: true})
+
+
+
+export const Post = mongoose.model<PostDocumentModel>(POST_COLLECTION_NAME, postSchema);
