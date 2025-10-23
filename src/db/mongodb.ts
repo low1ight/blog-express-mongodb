986241aaa94000c98d4f504fde3a5f563b3e5835
Maksym <qlowlight@gmail.com>
2025-10-23 07:34:24 +0300

    remove old user collections

--- src/db/mongodb.ts
+++ src/db/mongodb.ts
@@ -1,6 +1,5 @@
 import {Collection, Db, MongoClient} from 'mongodb';
 import {SETTINGS} from "../settings";
-import {UserDocumentModel} from "../modules/users_module/users/models/user-document-model";
 import {DeviceDocumentModel} from "../modules/users_module/devices/models/device-document-model";
 import {LikeForCommentDocumentModel} from "../modules/blog_platform/comments/models/likeForComment-document-model";
 import {LikeForPostDocumentModel} from "../modules/blog_platform/posts/models/likeForPost-document-model";
@@ -9,7 +8,6 @@ import mongoose from "mongoose";
 
 
 
-const USER_COLLECTION_NAME = 'users';
 
 const LIKE_FOR_COMMENT_COLLECTION_NAME = 'likeForComments';
 const LIKE_FOR_POST_COLLECTION_NAME = 'likeForPost';
@@ -18,8 +16,6 @@ const DEVICE_COLLECTION_NAME = 'devices';
 
 export let client: MongoClient;
 export let likeForPostCollection: Collection<LikeForPostDocumentModel>;
-export let userCollection: Collection<UserDocumentModel>;
-
 export let likeForCommentCollection: Collection<LikeForCommentDocumentModel>;
 export let devicesCollection: Collection<DeviceDocumentModel>;
 
@@ -33,7 +29,6 @@ export async function runDB(url: string): Promise<void> {
 
 
     likeForPostCollection = db.collection<LikeForPostDocumentModel>(LIKE_FOR_POST_COLLECTION_NAME);
-    userCollection = db.collection<UserDocumentModel>(USER_COLLECTION_NAME);
     likeForCommentCollection = db.collection<LikeForCommentDocumentModel>(LIKE_FOR_COMMENT_COLLECTION_NAME);
     devicesCollection = db.collection<DeviceDocumentModel>(DEVICE_COLLECTION_NAME);
 
