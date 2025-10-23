a21e6ff66396cbc22f142bf47a010e26eb6f1a69
Maksym <qlowlight@gmail.com>
2025-10-23 08:00:11 +0300

    remove old likeForComment collection

--- src/db/mongodb.ts
+++ src/db/mongodb.ts
@@ -1,7 +1,6 @@
 import {Collection, Db, MongoClient} from 'mongodb';
 import {SETTINGS} from "../settings";
 import {DeviceDocumentModel} from "../modules/users_module/devices/models/device-document-model";
-import {LikeForCommentDocumentModel} from "../modules/blog_platform/comments/models/likeForComment-document-model";
 import {LikeForPostDocumentModel} from "../modules/blog_platform/posts/models/likeForPost-document-model";
 import mongoose from "mongoose";
 
@@ -9,14 +8,13 @@ import mongoose from "mongoose";
 
 
 
-const LIKE_FOR_COMMENT_COLLECTION_NAME = 'likeForComments';
+
 const LIKE_FOR_POST_COLLECTION_NAME = 'likeForPost';
 const DEVICE_COLLECTION_NAME = 'devices';
 
 
 export let client: MongoClient;
 export let likeForPostCollection: Collection<LikeForPostDocumentModel>;
-export let likeForCommentCollection: Collection<LikeForCommentDocumentModel>;
 export let devicesCollection: Collection<DeviceDocumentModel>;
 
 
@@ -29,7 +27,6 @@ export async function runDB(url: string): Promise<void> {
 
 
     likeForPostCollection = db.collection<LikeForPostDocumentModel>(LIKE_FOR_POST_COLLECTION_NAME);
-    likeForCommentCollection = db.collection<LikeForCommentDocumentModel>(LIKE_FOR_COMMENT_COLLECTION_NAME);
     devicesCollection = db.collection<DeviceDocumentModel>(DEVICE_COLLECTION_NAME);
 
 
